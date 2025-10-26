361153ea70a9622989447d5b2c766e77c97bb7d6
Daniel Calebe <160525339+danielcalebe@users.noreply.github.com>
2025-10-26 13:35:54 -0300

    Refactor database connection to use environment variables

--- ecoraiz-adm/db.php
+++ ecoraiz-adm/db.php
@@ -1,12 +1,25 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ecoraiz";
+$host = getenv('ecoraiz-danielcalebe719-2b82.f.aivencloud.com');      // host do Aiven
+$user = getenv('avnadmin');      // usuário do Aiven
+$pass = getenv('AVNS_sexjgLJAxB2JrKZ_eQH');      // senha
+$db   = getenv('defaultdb');      // nome do banco
+$port = getenv('25538') ?: 3306; // porta padrão 3306
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$mysqli = mysqli_init();
 
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+// Se precisar de SSL (recomendado pelo Aiven)
+$ssl_ca   = getenv('DB_SSL_CA');
+$ssl_cert = getenv('DB_SSL_CERT');
+$ssl_key  = getenv('DB_SSL_KEY');
+
+if ($ssl_ca && $ssl_cert && $ssl_key) {
+    $mysqli->ssl_set($ssl_key, $ssl_cert, $ssl_ca, null, null);
 }
-?>
+
+$mysqli->real_connect($host, $user, $pass, $db, $port);
+
+if ($mysqli->connect_error) {
+    die("Erro de conexão: " . $mysqli->connect_error);
+}
+
+echo "Conexão MySQL Aiven com mysqli bem-sucedida!";
