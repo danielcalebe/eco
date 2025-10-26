14a1b1ff7549cbf0e185d4fdee0fec2e9acb68f1
Daniel Calebe <160525339+danielcalebe@users.noreply.github.com>
2025-10-26 13:50:23 -0300

    Refactor database connection settings
    Updated database connection details to use hardcoded values instead of environment variables. Removed SSL configuration code.

--- public/db.php
+++ public/db.php
@@ -1,21 +1,11 @@
 <?php
-$host = getenv('ecoraiz-danielcalebe719-2b82.f.aivencloud.com');      // host do Aiven
-$user = getenv('avnadmin');      // usuário do Aiven
-$pass = getenv('AVNS_sexjgLJAxB2JrKZ_eQH');      // senha
-$db   = getenv('defaultdb');      // nome do banco
-$port = getenv('25538');// porta padrão 3306
+$host = 'ecoraiz-danielcalebe719-2b82.f.aivencloud.com';
+$port = 25538; // porta do Aiven
+$user = 'avnadmin';
+$pass = 'AVNS_sexjgLJAxB2JrKZ_eQH';
+$db   = 'defaultdb';
 
 $mysqli = mysqli_init();
-
-// Se precisar de SSL (recomendado pelo Aiven)
-$ssl_ca   = getenv('DB_SSL_CA');
-$ssl_cert = getenv('DB_SSL_CERT');
-$ssl_key  = getenv('DB_SSL_KEY');
-
-if ($ssl_ca && $ssl_cert && $ssl_key) {
-    $mysqli->ssl_set($ssl_key, $ssl_cert, $ssl_ca, null, null);
-}
-
 $mysqli->real_connect($host, $user, $pass, $db, $port);
 
 if ($mysqli->connect_error) {
