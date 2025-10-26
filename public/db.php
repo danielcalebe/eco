23ace133b12c8ab3c068bc056c5d49aa06aecf27
Daniel Calebe <160525339+danielcalebe@users.noreply.github.com>
2025-10-26 14:04:55 -0300

    Enhance MySQL connection handling in db.php

--- public/db.php
+++ public/db.php
@@ -1,15 +1,25 @@
 <?php
+// db.php - Conexão mysqli com MySQL do Aiven
+
 $host = 'ecoraiz-danielcalebe719-2b82.f.aivencloud.com';
 $port = 25538; // porta do Aiven
 $user = 'avnadmin';
 $pass = 'AVNS_sexjgLJAxB2JrKZ_eQH';
 $db   = 'defaultdb';
 
+// Inicializa e conecta
 $mysqli = mysqli_init();
 $mysqli->real_connect($host, $user, $pass, $db, $port);
 
+// Checa se houve erro na conexão
 if ($mysqli->connect_error) {
     die("Erro de conexão: " . $mysqli->connect_error);
 }
 
-echo "Conexão MySQL Aiven com mysqli bem-sucedida!";
+// Define charset UTF-8
+$mysqli->set_charset("utf8");
+
+// Criar segunda variável $conn apontando para a mesma conexão
+$conn = $mysqli;
+
+echo "Conexão MySQL Aiven com mysqli e conn bem-sucedida!";
